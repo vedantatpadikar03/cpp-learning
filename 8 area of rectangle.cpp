6a807bd552925efdee5af9a6b22b4b61dbbd552e
Vedant Atpadikar 03 <vedantatpadikar@gmail.com>
2026-08-25 22:58:53 +0530

    Add rectangle area program

---
+++ 8 area of rectangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // write length below
+    int length = 5;
+    // write width below
+    int width = 4;
+    // formula of area
+    int area = length * width;
+    // display output 
+    cout<< area ;
+    return 0;
+}
