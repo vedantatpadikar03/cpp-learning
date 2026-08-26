24fb0806b48d838107d6cf5477ecf23e7cdb0166
Vedant Atpadikar 03 <vedantatpadikar@gmail.com>
2026-08-26 21:33:20 +0530

    Add simple calculator

---
+++ 9 creating simple calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+ {
+  int x, y;
+  int sum;
+  cout << "Type a number: ";
+  cin >> x;
+  cout << "Type another number: ";
+  cin >> y;
+  sum = x + y;
+  //print the total sum
+  cout << "Sum is: " << sum;
+  return 0;
+}
