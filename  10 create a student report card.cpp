a81e809837c57763c0e7ae0fde21bb4418b40c3a
Vedant Atpadikar 03 <vedantatpadikar@gmail.com>
2026-08-26 22:16:55 +0530

    Add student report card program

---
+++  10 create a student report card.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // Write studentID here
+int studentID = 1001;
+ // Write score here
+  float score = 92.5;
+ // Write score here
+  char grade = 'A';
+ // write message here
+string greeting = "hello";
+ // behaviour value
+bool value= 1;
+
+  // Print studentID
+cout << studentID << "\n";
+// Print score
+  cout << score << "\n";
+// Print grade
+  cout << grade << "\n";
+// print message
+cout<< greeting << "\n";
+//print value
+cout<< value ;
+  return 0;
+}
+ 
