f5b30ce39c8f28ac88b6fe8aa95cca937eaff5ea
Vedant Atpadikar 03 <vedantatpadikar@gmail.com>
2026-08-27 15:47:12 +0530

    Add calculating total cost program

---
+++ 11 calculating total cost of item.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int itemPrice = 75;
+    int shippingCost= 25;
+// total sum that customer should pay
+    int sum = itemPrice + shippingCost;
+      
+    // display output
+    cout<< sum;
+
+    return 0;
+}
