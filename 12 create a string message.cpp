28e4644c0116405dfba4f76eecf71adf7c4be2c4
Vedant Atpadikar 03 <vedantatpadikar@gmail.com>
2026-08-27 16:02:49 +0530

    Add string message program

---
+++ 12 create a string message.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string message = "this is casual program because recently i strated to doing coding means i am learning c language first time";
+     // displaying the message
+      cout<< message;
+      return 0;
+}
